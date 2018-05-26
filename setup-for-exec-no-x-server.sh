dd527754d864d16744798224b2030b71dd017035
Dominique RIGHETTO <dominique.righetto@gmail.com>
2018-05-26 11:27:53 +0200

    Try to fix the issue on CI build

---
+++ setup-for-exec-no-x-server.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# See https://github.com/JazzCore/python-pdfkit/wiki/Using-wkhtmltopdf-without-X-server
+printf '#!/bin/bash\nxvfb-run -a --server-args="-screen 0, 1024x768x24" /usr/bin/wkhtmltopdf -q $*' > /usr/bin/wkhtmltopdf.sh
+chmod a+x /usr/bin/wkhtmltopdf.sh
+ln -s /usr/bin/wkhtmltopdf.sh /usr/local/bin/wkhtmltopdf
